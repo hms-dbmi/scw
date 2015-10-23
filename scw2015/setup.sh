fe0603210e131d761d7ae78be8153705da76ff5b
1j <herman@stats.ox.ac.uk>
2015-10-23 14:51:45 -0400

    Made setup script slightly more general, so that people can download and re-run on other systems.

--- scw2015/setup.sh
+++ scw2015/setup.sh
@@ -1,3 +1,13 @@
-module load stats/R/3.2.1
-#export PATH=/opt/bcbio/local/bin:/groups/bcbio/bcbio/anaconda/bin/:$PATH
-export R_LIBS=/groups/pklab/scw2014/lib
+# This file should be 'source'd from
+# the directory in which it resides.
+domain=$( domainname )
+if [ "$domain"=="Orchestra" ]; then
+    module load stats/R/3.2.1
+    export R_LIBS=/groups/pklab/scw2014/lib
+    echo "Using pre-installed R libraries."
+else 
+    echo "You will need to install the following R libraries:"
+    cat required_libraries.txt
+fi
+export SCW_HOME=$( pwd )
+
