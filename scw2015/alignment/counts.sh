2426f20b6257b00ea1da38e30799624ba3b9517a
Radhika Khetani <khetani.r@gmail.com>
2015-11-02 14:05:41 -0500

    added 2 scripts for quick completion

--- scw2015/alignment/counts.sh
+++ scw2015/alignment/counts.sh
@@ -1,10 +1,12 @@
 #!/bin/bash/
 
-set -x 
+set -x
 
 #counts
-	module load seq/subread/1.4.6-p3			#featureCounts is part of the subread package
-	    
-	featureCounts --primary -a /groups/shared_databases/igenome/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf -o combined.featureCounts L139_ESC_1-tophat/L139_ESC_1.bam L139_ESC_2-tophat/L139_ESC_2.bam L139_MEF_49-tophat/L139_MEF_49.bam L139_MEF_50-tophat/L139_MEF_50.bam
-	sed 1d combined.featureCounts | cut -f1,7- | sed s/Geneid/id/ > combined.counts
+        module load seq/subread/1.4.6-p3                        #featureCounts is part of the subread package
+
+        cp /groups/pklab/scw/scw2015/ES.MEF.data/backup_data/bams/* .
+    
+        featureCounts --primary -a /groups/shared_databases/igenome/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf -o combined.featureCounts L139_ESC_1.subset.bam L139_ESC_2.subset.bam L139_MEF_49.subset.bam L139_MEF_50.subset.bam
+        sed 1d combined.featureCounts | cut -f1,7- | sed s/Geneid/id/ > combined.counts
 
