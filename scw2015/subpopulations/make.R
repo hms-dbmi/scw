fc3fe954f993fe8477000e34e066370e67de6126
1j <herman@stats.ox.ac.uk>
2015-11-03 18:19:52 -0500

    Removing additional files to avoid clutter.

--- scw2015/subpopulations/make.R
+++
@@ -1,6 +0,0 @@
-#library(knitr)
-#knitr::knit2html('analysis_of_heterogeneity_and_subpopulations.Rmd')
-# knitr still uses R Markdown v1, which is uglier
-# use V2 to integrate Pandoc and Bootstrap themes
-library(rmarkdown)
-rmarkdown::render('analysis_of_heterogeneity_and_subpopulations.Rmd', 'html_document')
