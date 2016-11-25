408c4b2d930474dd7a91ccff01294a802ebd8c77
Radhika <khetani.r@gmail.com>
2016-11-25 17:39:09 -0500

    adding the setup script

---
+++ scw2016/setup.sh
@@ -0,0 +1,14 @@
+# This file should be 'source'd from
+# the directory in which it resides.
+domain=$( domainname )
+if [ "$domain"=="Orchestra" ]; then
+    module load stats/R/3.2.1-Cairo
+    module load dev/python/2.7.6
+    export R_LIBS=/groups/pklab/scw/scw2015/lib
+    echo "Using pre-installed R libraries."
+else 
+    echo "You will need to install the following R libraries:"
+    cat required_libraries.txt
+fi
+export SCW_HOME=$( pwd )
+
